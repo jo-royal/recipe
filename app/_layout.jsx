bf507507198c071213faa1d378ef15809cb4d966
James O <51519722+jo-royal@users.noreply.github.com>
2025-07-30 19:39:34 +0200

    chore: update package.json and add tailwind.config.js
    - Changed main entry point to "expo-router/entry".
    - Added new dependencies: axios, expo-constants, expo-linking, expo-router, nativewind, prettier-plugin-tailwindcss, react-native-reanimated, react-native-safe-area-context, react-native-screens, and tailwindcss.
    - Created tailwind.config.js for Tailwind CSS configuration with NativeWind preset.

---
+++ app/_layout.jsx
@@ -0,0 +1,13 @@
+import '../global.css';
+import { View, Text } from 'react-native'
+
+
+const _layout = () => {
+    return (
+        <View className="flex-1 items-center justify-center">
+            <Text className="font-black">Hello World</Text>
+        </View>
+    )
+}
+
+export default _layout
---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+
+
+const index = () => {
+    return (
+        <View>
+            <Text className="font-black">index</Text>
+        </View>
+    )
+}
+
+export default index
