85764140d1fdabe685c2f9ba6285f5073e09de5d
James O <51519722+jo-royal@users.noreply.github.com>
2025-07-31 19:50:13 +0200

    Implement code changes to enhance functionality and improve performance

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,11 @@ import { Link } from 'expo-router'
 import { View, Image, Text, useColorScheme, SafeAreaView, ScrollView } from 'react-native'
 import Colors from '../constants/colors';
 import avater from '../assets/avater.jpg';
+import breakfast from '../assets/breakfast-cat.png';
+import beef from '../assets/beef-cat.png';
+import chicken from '../assets/chicken-cat.png';
+import dessert from '../assets/dessert-cat.png';
+import all from '../assets/all-cat.png';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Index = () => {
@@ -37,7 +42,38 @@ const Index = () => {
                 </View>
             </View>
 
-            <ScrollView></ScrollView>
+            <View className="mt-5 px-5 w-full flex-row justify-between items-center">
+                <View className="flex-col gap-3 items-center">
+                    <View className="p-5 w-fit h-fit rounded-full bg-gray-200">
+                        <Image className="w-10 h-10 rounded-full" source={breakfast}></Image>
+                    </View>
+                    <Text className="font-semibold">Breakfast</Text>
+                </View>
+                <View className="flex-col gap-3 items-center">
+                    <View className="p-5 w-fit h-fit rounded-full bg-gray-200">
+                        <Image className="w-10 h-10 rounded-full" source={beef}></Image>
+                    </View>
+                    <Text className="font-semibold">Beef</Text>
+                </View>
+                <View className="flex-col gap-3 items-center">
+                    <View className="p-5 w-fit h-fit rounded-full bg-gray-200">
+                        <Image className="w-10 h-10 rounded-full" source={chicken}></Image>
+                    </View>
+                    <Text className="font-semibold">Chicken</Text>
+                </View>
+                <View className="flex-col gap-3 items-center">
+                    <View className="p-5 w-fit h-fit rounded-full bg-gray-200">
+                        <Image className="w-10 h-10 rounded-full" source={dessert}></Image>
+                    </View>
+                    <Text className="font-semibold">Dessert</Text>
+                </View>
+                <View className="flex-col gap-3 items-center">
+                    <View className="p-5 w-fit h-fit rounded-full bg-gray-200">
+                        <Image className="w-10 h-10 rounded-full" source={all}></Image>
+                    </View>
+                    <Text className="font-semibold">All</Text>
+                </View>
+            </View>
 
         </SafeAreaView>
     )
