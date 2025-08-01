37887ea9265c33daec0a85136147feb8066a3e7f
James O <51519722+jo-royal@users.noreply.github.com>
2025-08-01 21:19:16 +0200

    Refactor layout component to use Tabs instead of Stack for improved navigation and styling consistency

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,23 +1,48 @@
-import { Stack } from 'expo-router';
+import { Tabs } from 'expo-router';
 import '../global.css';
+import { useColorScheme } from "react-native"
+import Colors from '../constants/colors';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 
 const RootLayout = () => {
-
+    const colorScheme = useColorScheme();
+    const theme = Colors[colorScheme || 'light'];
 
     return (
-        <Stack screenOptions={{
+        <Tabs screenOptions={{
             headerStyle: {
                 backgroundColor: '#f4511e',
             },
             headerTintColor: '#fff',
             headerTitleStyle: {
                 fontWeight: 'bold',
-            }
+            },
+
+            tabBarStyle: { backgroundColor: theme.surface, paddingTop: 10, height: 90 },
+            tabBarActiveTintColor: theme.primary,
+            tabBarInactiveTintColor: theme.accent,
+
         }}>
-            <Stack.Screen name="index" options={{ headerShown: false, title: 'Home' }} />
-            <Stack.Screen name="details" options={{ title: 'Details' }} />
-        </Stack>
+            <Tabs.Screen name="index" options={{
+                headerShown: false, title: 'Home', tabBarIcon: ({ focused }) => (
+                    <Ionicons
+                        size={24}
+                        name={focused ? 'home' : 'home-outline'}
+                        color={focused ? theme.primary : theme.accent}
+                    />
+                )
+            }} />
+            <Tabs.Screen name="details" options={{
+                title: 'Details', tabBarIcon: ({ focused }) => (
+                    <Ionicons
+                        size={24}
+                        name={focused ? 'apps' : 'apps-outline'}
+                        color={focused ? theme.primary : theme.accent}
+                    />
+                )
+            }} />
+        </Tabs>
 
     )
 }
